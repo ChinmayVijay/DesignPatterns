2cc09d5f8d2479b81f934aaee712d37163c50677
Chinmay <ichinmay08@gmail.com>
2020-07-06 14:59:07 +0530

    Design Patterns | Chap Two | Weather Station | Observer Pattern | DP#3 | Loose Coupling

--- main/java/chap_two/WeatherData.java
+++ main/java/chap_two/WeatherData.java
@@ -2,6 +2,22 @@ package chap_two;
 
 import java.util.ArrayList;
 
+/**
+ * The Observer Pattern defines the One-To-Many relationship between objects, so that
+ * when one objects(Subject) state changes all of its dependents are notified and updated automatically.
+ *
+ *
+ * In our case : WeatherData acts like a Subject for all the display elements.
+ *
+ * Design Principle :
+ * "Strive for loosely coupled designs between objects that interact."
+ *
+ * Simpler Terms : Loosely coupled designs allow us to build flexible OO systems that can handle
+ * change because they minimize the interdependency between objects.
+ *
+ *
+ *
+ */
 public class WeatherData implements Subject {
     private ArrayList<Observer> observers;
     private float temperature;
